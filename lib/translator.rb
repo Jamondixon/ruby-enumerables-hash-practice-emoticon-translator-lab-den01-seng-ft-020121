fce2d83bdb1e7357c681a345ff37702c543935f3
Jamon Dixon <jamonadixon@gmail.com>
2021-01-23 01:03:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,15 +16,18 @@ require 'yaml'
 
 
 
-  def get_japanese_emoticon (path, emoticon) #takes Western emoticon, :) and translate it to Japanese emoticon
-    if load_library(file_path)[:get_emoticon][emoticon]
-    load_library(file_path)[:get_emoticon][emoticon]
-    else "Sorry, that emoticon was not found"
+  def get_japanese_emoticon (path, emoticon) 
+     load_library(path).each do |key, value|
+        if value[:english] == emoticon
+          return value[:japanese]
+        end
     end
+    return "Sorry, that emoticon was not found"
   end
+  
 
 
-  def get_english_meaning (path, emoticon) #takes a Japanese emoticon and returns its meaning in English
+  def get_english_meaning (path, emoticon) 
      load_library(path).each do |key, value|
        if value[:japanese] == emoticon
          return key
