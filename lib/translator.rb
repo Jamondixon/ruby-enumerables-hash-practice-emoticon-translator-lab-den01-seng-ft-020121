6a0ca5212d9bb57ceb8179a614aa7cc9abc2683c
Jamon Dixon <jamonadixon@gmail.com>
2021-01-23 00:50:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,3 +13,23 @@ require 'yaml'
     end
     final_hash
   end
+
+
+  def get_japanese_emoticon(path, emoticon)
+    emoji = load_library(path)
+    if emoji["get_emoticon"].has_key?(emoticon)
+      emoji["get_emoticon"][emoticon]
+    else
+      return "Sorry, that emoticon was not found"
+    end
+  end
+
+
+  def get_english_meaning(path, emoticon)
+    emoji = load_library(path)
+    if emoji["get_meaning"].has_key?(emoticon)
+      emoji["get_meaning"][emoticon]
+    else
+      return "Sorry, that emoticon was not found"
+    end
+  end
